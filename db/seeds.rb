ca774cd5fae05b90668fa112fc23acbf202e7652
Pablo Baldini <pablo.baldini@gmail.com>
2020-02-20 01:03:35 +0000

    Ingredient added

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,5 @@ user = JSON.parse(user_serialized)
 user["drinks"].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
   Ingredient.create(name: "Salt")
+  Ingredient.create(name: "Coca Cola")
 end
